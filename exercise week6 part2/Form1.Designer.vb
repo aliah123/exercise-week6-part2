5285e6698825d7bf5a2d15086cbb3bc7245cb58b
aliah123 <82350806+aliah123@users.noreply.github.com>
2021-04-17 14:28:04 +0800

    Add files via upload

---
+++ exercise week6 part2/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblNamaHari = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListName = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lblNamaHari
+        '
+        Me.lblNamaHari.AutoSize = True
+        Me.lblNamaHari.Location = New System.Drawing.Point(56, 171)
+        Me.lblNamaHari.Name = "lblNamaHari"
+        Me.lblNamaHari.Size = New System.Drawing.Size(51, 17)
+        Me.lblNamaHari.TabIndex = 0
+        Me.lblNamaHari.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(277, 168)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListName
+        '
+        Me.ListName.FormattingEnabled = True
+        Me.ListName.ItemHeight = 16
+        Me.ListName.Location = New System.Drawing.Point(478, 146)
+        Me.ListName.Name = "ListName"
+        Me.ListName.Size = New System.Drawing.Size(120, 148)
+        Me.ListName.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListName)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lblNamaHari)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblNamaHari As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListName As ListBox
+End Class
---
+++ exercise week6 part2/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblNamaHari.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strNamaHari(6) As String
+        Dim nilai As Integer
+        strNamaHari(0) = "Ahad"
+        strNamaHari(1) = "Isnin"
+        strNamaHari(2) = "Selasa"
+        strNamaHari(3) = "Rabu"
+        strNamaHari(4) = "Khamis"
+        strNamaHari(5) = "Jumaat"
+        strNamaHari(6) = "Sabtu"
+
+        Array.Reverse(strNamaHari)
+        For x = 0 To 6
+            ListName.Items.Add(strNamaHari(x))
+        Next
+
+        lblNamaHari.Text = strNamaHari(6)
+    End Sub
+End Class
